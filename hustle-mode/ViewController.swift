920c373961b15d02bae8fae82e18d0d132a2766d
Jennifer Mah <jema5807@colorado.edu>
2019-02-07 10:18:09 -0700

    Initial Commit

---
+++ hustle-mode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by Jennifer Mah on 2/7/19.
+//  Copyright © 2019 Jennifer Mah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
