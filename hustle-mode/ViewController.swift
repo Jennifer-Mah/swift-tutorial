b7f70f128c39c5756473b886d79d00ec934ec66e
Jennifer Mah <jema5807@colorado.edu>
2019-02-12 22:01:02 -0700

    first commit

--- hustle-mode/ViewController.swift
+++ hustle-mode/ViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+//  Import audio playing
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var darkBlueBG: UIImageView!
+    @IBOutlet weak var powerBtn: UIButton!
+    @IBOutlet weak var couldHolder: UIView!
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var hustleLbl: UILabel!
+    @IBOutlet weak var onLbl: UILabel!
+    
+    var player: AVAudioPlayer!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
+
+        
     }
 
 
+    @IBAction func powerBtnPressed(_ sender: Any) {
+        couldHolder.isHidden=false
+        darkBlueBG.isHidden=true
+        powerBtn.isHidden=true
+        
+        player.play()
+        
+        UIView.animate(withDuration: 2.3, animations: {
+            self.rocket.frame = CGRect(x: 0, y: 140, width: 375, height: 402)
+        }) { (finished) in
+            self.hustleLbl.isHidden = false
+            self.onLbl.isHidden = false
+        }
+    }
+    
+    
+    
+    
 }
 
